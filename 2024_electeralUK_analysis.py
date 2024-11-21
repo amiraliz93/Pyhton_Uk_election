fcdba2959389add8b6f6c1b7759f36305715bd81
N1259534 <amir.alizadeh2023@my.ntu.ac.uk>
2024-11-21 21:38:59 +0000

    Create 2024_electeralUK_analysis.py

---
+++ 2024_electeralUK_analysis.py
@@ -0,0 +1,10 @@
+def display_menu():
+    print("\n--- Voting Analysis Program ---")
+    print("1. View Candidate Information")
+    print("2. Display Total Votes for a Party")
+    print("3. Display Constituency Information")
+    print("4. Calculate Vote Percentage for a Party in a Constituency")
+    print("5. Save Statistics to a File")
+    print("6. Exit")
+
+    
