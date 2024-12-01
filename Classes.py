d34e049282d86735816084ab827d8a6ab1626602
N1259534 <N1259534@my.ntu.ac.uk>
2024-12-01 14:21:40 +0000

    add classes all to one place

--- Classes.py
+++ Classes.py
@@ -1,3 +1,47 @@
+
+
+
+class MP:
+    def __init__(self, f_name, l_name, gender, party, votes):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.candidate_full_name = (self.f_name) + ' ' +(self.l_name)
+        self.party = party
+        self.votes = votes if votes is not None else 0
+        self.gender = gender
+    def __str__(self): # need to change
+        return f"{self.f_name}  {self.l_name},\nParty: {self.party},\nVotes: {self.votes},\nGender: {self.gender}"
+
+    def get_candidate_summary(self):
+        return {
+            'Full Name': self.candidate_full_name,
+            'Party': self.party,
+            'Votes': self.votes,
+            'Gender': self.gender
+        }
+
+class Party:
+     def __init__(self, P_name):
+          self.P_name = P_name
+          self.total_selected_votes = 0
+          self.total_votes = 0
+          self.members = []
+        
+     def __str__(self):
+       return f' {self.P_name} \n Number of parliament seats that were achieved {len(self.members)} \n Member: \n {self.members} '
+     
+
+     def add_vote_of_selected(self, votes):
+        self.total_selected_votes += votes
+     
+     def calculate_avg_vote(self):
+          self.total_votes / len(self.members) if len(self.members) >0 else 0
+
+
+     def add_member_party(self, member):
+          if member not in self.members:
+            self.members.append(member)
+
 class Constituency:
     def __init__(self, constituency_name, region_name, country_name, Constituency_type, total_vote):
         self.C_name = constituency_name
